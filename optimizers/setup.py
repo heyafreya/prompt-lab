0d229d35cdfe39c76ccd1e61a4e2539152166c30
heyafreya <fnair3@gatech.edu>
2026-05-08 22:04:04 -0400

    feat: add auto docs and changelog generation

--- optimizers/setup.py
+++ optimizers/setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="optimizers",
-    version="0.1",
+    version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
 )
